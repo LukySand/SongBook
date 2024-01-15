813c5431fbc468ba66eb17e53d33d32d1a26926b
LukySand <luky.sand@gmail.com>
2024-01-15 15:30:48 -0300

    added the SongArray

---
+++ src/utils/SongArray.js
@@ -0,0 +1,130 @@
+export default SongArray = [
+  {
+    title: "Abre mis ojos oh Cristo",
+    lyrics: [
+      {
+        type: "standard",
+        paragraph: [
+          {
+            text: "Abre mis ojos Oh Cristo,",
+            chords: [{ note: "D", position: 0 }],
+          },
+          {
+            text: "Abre mis ojos, Señor,",
+            chords: [{ note: "A", position: 0 }],
+          },
+          {
+            text: "yo quiero verte,",
+            chords: [{ note: "G", position: 11 }],
+          },
+          {
+            text: "yo quiero verte. [Repetir]",
+            chords: [{ note: "D", position: 11 }],
+          },
+        ],
+      },
+      {
+        type: "chorus",
+        paragraph: [
+          {
+            text: "Quiero ver tu majestad",
+            chords: [
+              { note: "A", position: 7 },
+              { note: "Bm", position: 19 },
+            ],
+          },
+          {
+            text: "brillando sobre toda la tierra,",
+            chords: [
+              { note: "Em", position: 1 },
+              { note: "A", position: 26 },
+            ],
+          },
+          {
+            text: "sentir tu gran poder y amor,",
+            chords: [
+              { note: "A7", position: 1 },
+              { note: "Bm", position: 25 },
+            ],
+          },
+          {
+            text: "yo quiero estar en tu presencia.",
+            chords: [
+              { note: "G", position: 13 },
+              { note: "A", position: 26 },
+            ],
+          },
+        ],
+      },
+      {
+        type: "standard",
+        paragraph: [
+          {
+            text: "Santo, Santo, Santo,(Eres)",
+            chords: [{ note: "D", position: 0 }],
+          },
+          {
+            text: "Santo, Santo, Santo,(Eres)",
+            chords: [{ note: "A", position: 0 }],
+          },
+          {
+            text: "Santo, Santo, Santo,(Eres)",
+            chords: [{ note: "G", position: 0 }],
+          },
+          {
+            text: "Yo quiero verte.",
+            chords: [{ note: "D", position: 11 }],
+          },
+        ],
+      },
+    ],
+    number: 1,
+  },
+
+  {
+    title: "Al comenzar la semana",
+    lyrics: [
+      {
+        type: "standard",
+        paragraph: [
+          {
+            text: "Al comenzar la semana, ¡qué alegría que me da",
+            chords: [
+              { note: "G", position: 0 },
+              { note: "C", position: 18 },
+              { note: "G", position: 20 },
+              { note: "D", position: 44 },
+            ],
+          },
+          {
+            text: "el estar en nuestra casa en este bello lugar!",
+            chords: [
+              { note: "D", position: 21 },
+              { note: "D7", position: 25 },
+              { note: "G", position: 42 },
+            ],
+          },
+          {
+            text: "Enlacemos nuestras manos para juntos alabar",
+            chords: [
+              { note: "G", position: 0 },
+              { note: "C", position: 20 },
+              { note: "G", position: 41 },
+            ],
+          },
+          {
+            text: "el nombre de Jesucristo, nuestro amigo más leal.",
+            chords: [
+              { note: "C7", position: 0 },
+              { note: "G", position: 17 },
+              { note: "Em", position: 25 },
+              { note: "D", position: 40 },
+              { note: "G", position: 46 },
+            ],
+          },
+        ],
+      },
+    ],
+    number: 2,
+  },
+];
